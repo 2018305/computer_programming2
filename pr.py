e9766a75b4ec9f11f3977da4eb891f99916894e8
2018305 <suk8949123@gmail.com>
2023-09-22 20:34:37 +0900

    4주차 퀴즈
    4주차 퀴즈입니다.

---
+++ pr.py
@@ -0,0 +1,16 @@
+a = [90, 45, 64, 9, 17, 29]
+result = []
+for i in a:
+  if i >= 71:
+    result.append("A")
+  elif i >= 41:
+    result.append("B")
+  elif i >= 11:
+   result.append("C")
+  else:
+   result.append("D")
+print(result)
+
+
+
+
