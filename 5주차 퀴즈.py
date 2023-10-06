f86d67314f7acc2b6fb9599a6e395c8788748e06
2018305 <suk8949123@gmail.com>
2023-10-06 20:53:53 +0900

    5주차 퀴즈
    5주차 퀴즈입니다.

---
+++ 5주차 퀴즈.py
@@ -0,0 +1,11 @@
+def ggdan(n):
+    for x in range(1,n+1):
+        print("------[" + str(x) + "단] ------")
+        for y in range(1,10):
+            print(x, "X", y, "=", x*y)
+print("----------------")
+
+num = int(input("몇 단까지 출력할까요?"))
+ggdan(num)
+
+
